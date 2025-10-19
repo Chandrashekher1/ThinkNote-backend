9c10336f6af64c5186cdff7248cee1d218307140
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-10-19 22:41:28 +0530

    feat: add startup directory

---
+++ src/startup/route.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import user from "../routes/user.route"
+import content from "../routes/content.route"
+import link from "../routes/link.route"
+import login from "../routes/auth.route"
+import { Application } from 'express';
+
+export default function(app: Application){
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.use('/user', user)
+    app.use('/content', content)
+    app.use('/link', link)
+    app.use('/login', login)
+
+}
